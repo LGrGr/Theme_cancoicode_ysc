4efedbd04a1ba6f1b96390f3d2d84208a99e50c3
fsimmet3 <floriane.simmet@gmail.com>
2018-01-25 11:31:52 +0100

    logo accueil taille

--- front-page.php
+++ front-page.php
@@ -9,17 +9,17 @@
 </head>
 <body <?php body_class(); ?> >
    <!------------------------------------HEADER-------------------------------------------->
-  
+
 <header class="container-fluid header_accueil">
     <div class="container">
         <div class="row text-center justify-content-center">
             <div class="">
-                <img src="<?php bloginfo('template_directory'); ?>/assets/img/logo_cancoicode_penche2.svg" alt="logo_cancoicode" class="img-fluid logo">
+                <img src="<?php bloginfo('template_directory'); ?>/assets/img/logo_cancoicode_penche2.svg" alt="logo_cancoicode" class="img-fluid logo" width="80%">
             </div>
         </div>
     </div>
-    <!-----------------------------------NAV BAR----------------------------------------------> 
-     
+    <!-----------------------------------NAV BAR---------------------------------------------->
+
           <nav class="navbar navbar-expand-lg navbar-toggleable-md navbar-light">
         <button class="navbar-toggler navbar-right navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -40,116 +40,116 @@
         ) );
       ?>
     </nav>
- 
-                 
+
+
 </header>
-   
+
    <!----------------------------------------SECTION BIENVENUE------------------------------------->
 <section class="section_bienvenue">
-    
+
     <div class="container-fluid">
         <div class="container">
             <div class="row">
                 <h2 class="titre_bienvenue">Bienvenue sur le blog de "/cancoicode"!</h2>
-                
+
             </div>
             <div class="row">
-               
+
                 <p class="sous_titre_bienvenue">La promo 2018 de l'acs Vesoul</p>
-                
+
                 <p class="paragraphe_bienv">Bienvenue sur le site du collectif "/cancoicode". Nous sommes 12 développeurs juniors venant de tous horizons. Sur le site, vous retrouverez nos articles de blog portant sur des thèmes très divers mais tous liés au numérique, les différents projets sur lesquels nous avons travaillés et surtout, la desription de chacun des membres du collectif et leurs travaux personnels.</p>
             </div>
-            
+
             <div class="row bonne_visite">
                 <p>Bonne visite !</p>
             </div>
-            
-        </div> 
+
+        </div>
     </div>
-    
+
 </section>
-   
+
    <!-----------------------------------------SECTION CARROUSSEL------------------------------------->
-   
-   
+
+
 <section class="section_carousel">
     <div class="container-fluid carouselred">
 
                 <div id="carouselExampleControls" class="carousel carouselred slide" data-ride="carousel">
                     <div class="carousel-inner" role="listbox">
-                       
+
                         <div class="carousel-item active">
-                          
+
                           <div class="container">
                               <div class="row">
                                   <div class="col-6">
-                                     <h2 class="titre_article_carousel">SPACE INVADERS</h2> 
+                                     <h2 class="titre_article_carousel">SPACE INVADERS</h2>
                                      <p class="sous-titre_art_car">Sous-titre d'article</p>
                                      <hr align="left" />
                                      <p class="paragraph_carr">Altera sententia est, quae definit amicitiam paribus officiis ac voluntatibus. Hoc Quidem est nimis exigue et exiliter ad calculos vocare amicitiam, ut par sit ratio acceptorum et datorum. Divitior mihi et affluentior videtur esse vera amicitia nex observare restricte, ne plus redar quam acceperit;neque enim verendum est, ne quid excudat, aut ne quid in[...]</p>
                                   </div>
-                             
+
                                    <div class="col-6">
                                       <img src="<?php bloginfo('template_directory'); ?>/assets/img/space.png">
                                   </div>
-                              </div> 
+                              </div>
                           </div>
                         </div>
-                                
-                                
+
+
                         <div class="carousel-item">
-                        
+
                          <div class="container">
                               <div class="row">
                                   <div class="col-6">
-                                     <h2 class="titre_article_carousel">SPACE INVADERS</h2> 
+                                     <h2 class="titre_article_carousel">SPACE INVADERS</h2>
                                      <p class="sous-titre_art_car">Sous-titre d'article</p>
                                      <hr align="left" />
                                      <p class="paragraph_carr">Altera sententia est, quae definit amicitiam paribus officiis ac voluntatibus. Hoc Quidem est nimis exigue et exiliter ad calculos vocare amicitiam, ut par sit ratio acceptorum et datorum. Divitior mihi et affluentior videtur esse vera amicitia nex observare restricte, ne plus redar quam acceperit;neque enim verendum est, ne quid excudat, aut ne quid in[...]</p>
                                   </div>
-                                  
+
                                    <div class="col-6">
                                       <img src="<?php bloginfo('template_directory'); ?>/assets/img/space.png">
                                   </div>
-                              </div> 
-                          </div>      
-                                 
-                            
+                              </div>
+                          </div>
+
+
                   </div>
-                  
+
                 <div class="carousel-item">
-                        
+
                          <div class="container">
                               <div class="row">
                                   <div class="col-6">
-                                     <h2 class="titre_article_carousel">SPACE INVADERS</h2> 
+                                     <h2 class="titre_article_carousel">SPACE INVADERS</h2>
                                      <p class="sous-titre_art_car">Sous-titre d'article</p>
                                          <hr align="left" />
                                      <p class="paragraph_carr">Altera sententia est, quae definit amicitiam paribus officiis ac voluntatibus. Hoc Quidem est nimis exigue et exiliter ad calculos vocare amicitiam, ut par sit ratio acceptorum et datorum. Divitior mihi et affluentior videtur esse vera amicitia nex observare restricte, ne plus redar quam acceperit;neque enim verendum est, ne quid excudat, aut ne quid in[...]</p>
                                   </div>
-                                  
-                                  
+
+
                                    <div class="col-6">
                                       <img src="<?php bloginfo('template_directory'); ?>/assets/img/space.png">
                                   </div>
-                                                        
-                            
+
+
                   </div>
 
                 </div>
             </div>
-        
-    
+
+
                     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="sr-only">Précédent</span>
                     </a>
-                    
+
                     <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="sr-only">Suivant</span>
                     </a>
-                    
+
             </div>
         </div>
     </div>
@@ -163,8 +163,8 @@
     <div class="container-fluid">
        <div class="container">
            <div class="row caroussel_articles">
-          
-              <?php 
+
+              <?php
 $args = array( 'post_type' => '', 'posts_per_page' => 3 );
 $loop = new WP_Query( $args );
 while ( $loop->have_posts() ) : $loop->the_post();
@@ -179,53 +179,53 @@ echo "<hr align='left' /> <p class='l'>".get_the_excerpt()."</p>       <p class=
 
 endwhile;
             wp_reset_postdata();?>
-            </div>  
+            </div>
            </div>
        </div>
 </section>
-   
-   
+
+
    <!----------------------------------------SECTION ICONES------------------------------------------>
-   
+
    <section class="section_icones">
        <div class="container-fluid icones">
           <div class="container">
               <div class="row text-center icones">
-                  
+
                  <div class="col-3">
-                     <img src="<?php bloginfo('template_directory'); ?>/assets/img/cafe.jpg" alt="icone d'un café" id="icone"> 
+                     <img src="<?php bloginfo('template_directory'); ?>/assets/img/cafe.jpg" alt="icone d'un café" id="icone">
                          <h4 class="text_icone count">2641</h4>
                          <h5 class="text_icone" >CAF&Eacute;S ENGLOUTIS</h5>
                      </div>
-              
-                 
+
+
                   <div class="col-3">
                      <img src="<?php bloginfo('template_directory'); ?>/assets/img/git.jpg" alt="icone de Github" id="icone">
                          <h4 class="text_icone count">991</h4>
                          <h5 class="text_icone">TENTATIVE DE PUSH</h5>
-                    
+
                  </div>
-                  
+
                    <div class="col-3">
-                     <img src="<?php bloginfo('template_directory'); ?>/assets/img/lit.jpg" alt="icone d'un lit" id="icone"> 
+                     <img src="<?php bloginfo('template_directory'); ?>/assets/img/lit.jpg" alt="icone d'un lit" id="icone">
                      <h4 class="text_icone count">195</h4>
                      <h5 class="text_icone">NUITS BLANCHES</h5>
                  </div>
-                  
+
                   <div class="col-3">
-                     <img src="<?php bloginfo('template_directory'); ?>/assets/img/couverts.jpg" alt="icone de couverts" id="icone"> 
+                     <img src="<?php bloginfo('template_directory'); ?>/assets/img/couverts.jpg" alt="icone de couverts" id="icone">
                      <h4 class="text_icone count">3471</h4>
                      <h5 class="text_icone">PANINIS LE MIDI</h5>
-                 </div> 
-                    </div>  
-                  
+                 </div>
+                    </div>
+
               </div>
-          </div> 
+          </div>
    </section>
-   
+
    <!-------------------------------CARROUSSEL EQUIPE-------------------------------------------->
    <section class="section_carouselEquipe">
-       
+
  <div class="container-fluid">
             <div id="carouselTeam" class="carousel slide" data-ride="carousel">
           <div class="carousel-inner">
@@ -261,6 +261,6 @@ endwhile;
           </a>
         </div>
     </div>
-       
+
    </section>
 <?php get_footer() ?>
